4d2687e2d0ee839d0a4f384735365f2c0e2933bc
student <sschubert@miners.utep.edu>
2021-02-14 19:17:26 -0700

    I added draw arrow and also the different fonts for drawing other charaters.

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = >) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -26,8 +26,19 @@ int main()
       break;
     case 'c':
       puts("You selected chars:");
+      puts("Font 11x16:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      puts("Font 8x12:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
+      puts("Font 5x7:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
+      break;
+    case '>':
+      puts("You selected arrow:");
+      print_arrow(5, 5);
       break;
     case 'q':
       puts("Bye!");
